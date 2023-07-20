7f6099f7ea994471c1f010c420dfc09c66b60ab0
aman-ids <aman@incredible-developers.com>
2023-07-20 16:33:21 +0530

    Fixes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,39 +14,37 @@ import {
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
-  
-  const showAlert = (message,type)=>{
+
+  const showAlert = (message, type)=>{
       setAlert({
-        msg : message,
-        type : type
+        msg: message,
+        type: type
       })
       setTimeout(() => {
-        setAlert(null);
+          setAlert(null);
       }, 1500);
   }
-  const toggleMode = ()=>{
 
+  const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode is enabled", "success")
+      showAlert("Dark mode has been enabled", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode is enabled", "success")
+      showAlert("Light mode has been enabled", "success");
     }
   }
   return (
     <>
-    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-    {/* <Navbar/> */}
     <Router>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alert alert= {alert}/>
       <div className="container my-3">
           <Routes>
-            <Route exact path="/about" element={<About />} />
+            <Route exact path="/about" element={<About mode={mode}/>} />
             <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
           </Routes>
       </div>
